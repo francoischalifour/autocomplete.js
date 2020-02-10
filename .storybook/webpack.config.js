835500e261f9224323795554c5e4c92bf8c3a85e
eunjae-lee <karis612@gmail.com>
2020-02-10 12:53:21 +0100

    chore: format webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -5,33 +5,33 @@ module.exports = ({ config, mode }) => {
   // `mode` can either be 'DEVELOPMENT' or 'PRODUCTION'
   // 'PRODUCTION' is used when building the static version of storybook.
 
-  config.module.rules.push({
-    test: /\.(js|ts|tsx)$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: 'babel-loader',
-      },
-    ],
-  });
-
-  config.module.rules.push({
-    test: /\.vue$/,
-    exclude: /node_modules/,
-    use: [
-      {
-        loader: 'vue-loader',
-      },
-    ],
-  });
+  config.module.rules.push(
+    {
+      test: /\.(js|ts|tsx)$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    {
+      test: /\.vue$/,
+      exclude: /node_modules/,
+      use: [
+        {
+          loader: 'vue-loader',
+        },
+      ],
+    }
+  );
 
   config.plugins.push(
     new webpack.DefinePlugin({
       __DEV__: JSON.stringify(mode === 'DEVELOPMENT'),
-    })
+    }),
+    new VueLoaderPlugin()
   );
-
-  config.plugins.push(new VueLoaderPlugin());
 
   config.resolve.extensions.push('.ts', '.tsx');
 
