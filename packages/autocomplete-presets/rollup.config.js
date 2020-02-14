56c0f029aa8603eadce72b6c22ad09a84ab455b1
Eunjae Lee <karis612@gmail.com>
2020-02-14 11:33:32 +0100

    chore: replace __DEV__ at building presets

--- packages/autocomplete-presets/rollup.config.js
+++ packages/autocomplete-presets/rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import replace from '@rollup/plugin-replace';
 import json from '@rollup/plugin-json';
 
 export default [
@@ -11,6 +12,9 @@ export default [
       sourcemap: true,
     },
     plugins: [
+      replace({
+        __DEV__: JSON.stringify(process.env.NODE_ENV === 'development'),
+      }),
       resolve({
         extensions: ['.js', '.ts', '.json'],
       }),
