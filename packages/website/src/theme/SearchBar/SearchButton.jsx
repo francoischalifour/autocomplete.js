3bf48556a37595a1ea5699ddb7ecd8dce8418f75
Shipow <shipowlata@gmail.com>
2020-03-10 20:19:04 +0100

    feat(docsearch): design search button

--- packages/website/src/theme/SearchBar/SearchButton.jsx
+++ packages/website/src/theme/SearchBar/SearchButton.jsx
@@ -4,7 +4,18 @@ import styles from './SearchButton.module.css';
 export default function SearchButton({ onClick }) {
   return (
     <button type="button" className={styles.button} onClick={onClick}>
-      <span className={styles.text}>Find anything</span>
+      <svg className={styles.icon} width="20px" viewBox="0 0 18 18">
+        <path
+          d="M13.14 13.14L17 17l-3.86-3.86A7.11 7.11 0 1 1 3.08 3.08a7.11 7.11 0 0 1 10.06 10.06z"
+          stroke="currentColor"
+          strokeWidth="1.78"
+          fill="none"
+          fillRule="evenodd"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+      <span className={styles.text}>Search docs...</span>
       <kbd className={styles.kbd}>Cmd+K</kbd>
     </button>
   );
