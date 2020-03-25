3d122fa603301af201b19424774e14ab1bed9efb
Shipow <shipowlata@gmail.com>
2020-03-25 11:31:15 +0100

    skin search button

--- packages/website/src/theme/SearchBar/SearchButton.jsx
+++ packages/website/src/theme/SearchBar/SearchButton.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import styles from './SearchButton.module.css';
 
 export default function SearchButton({ onClick }) {
   return (
-    <button type="button" className={styles.button} onClick={onClick}>
-      <svg className={styles.icon} width="20px" viewBox="0 0 18 18">
+    <button type="button" className="DocSearch-SearchButton" onClick={onClick}>
+      <svg className="DocSearch-SearchButton-Icon" width="18px" viewBox="0 0 18 18">
         <path
           d="M13.14 13.14L17 17l-3.86-3.86A7.11 7.11 0 1 1 3.08 3.08a7.11 7.11 0 0 1 10.06 10.06z"
           stroke="currentColor"
@@ -15,10 +14,10 @@ export default function SearchButton({ onClick }) {
           strokeLinejoin="round"
         ></path>
       </svg>
-        <span className={styles.text}>Search...</span>
-        <span className="DocSearch-SearchButton-Key">⌘</span>
-        <span className="DocSearch-SearchButton-KeySeparator">+</span>
-        <span className="DocSearch-SearchButton-Key">K</span>
+      <span className="DocSearch-SearchButton-Placeholder">Search...</span>
+      <span className="DocSearch-SearchButton-Key">⌘</span>
+      <span className="DocSearch-SearchButton-KeySeparator">+</span>
+      <span className="DocSearch-SearchButton-Key">K</span>
     </button>
   );
 }
