6863ca24f090cffe5d0aa6e2b86a6c74fa0d70f1
Shipow <shipowlata@gmail.com>
2020-03-28 11:11:50 +0100

    feat: animations

--- packages/docsearch-react/src/NoResults/NoResults.tsx
+++ packages/docsearch-react/src/NoResults/NoResults.tsx
@@ -5,5 +5,8 @@ interface NoResultsProps {
 }
 
 export function NoResults(props: NoResultsProps) {
-  return <p>No results for “{props.query}“.</p>;
+  return <div className="DocSearch-NoResults">
+    <div className="Docsearch-Hit-title">No results for “{props.query}“.</div>
+    <div className="DocSearch-Label">Try another search or if you believe this query should lead to actual results, please let us know with a <a href="">GitHub issue</a>.</div>
+  </div>;
 }
